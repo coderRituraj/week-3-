cbbfdb15ea0c512f8b95ff7c64174b79e7d98c01
coderRituraj <110223342+coderRituraj@users.noreply.github.com>
2022-08-19 15:54:01 +0530

    Crypto Demo project

---
+++ day3(18-08)/day5/cryptoScriot.js
@@ -0,0 +1,29 @@
+function bit(){
+    var price = prompt("Enter the amount");
+    var bit_price = 3000000;
+    var number = bit_price % price;
+    document.getElementById("btn1").innerHTML="Total quantity you can perchase is ="+number;
+   
+}
+
+function dog(){
+    var price = prompt("Enter the amount");
+    var bit_price = 3000;
+    var number = bit_price % price;
+    document.getElementById("btn2").innerHTML="Total quantity you can perchase is ="+number;
+   
+}
+function shibu(){
+    var price = prompt("Enter the amount");
+    var bit_price = 600;
+    var number = bit_price % price;
+    document.getElementById("btn3").innerHTML="Total quantity you can perchase is ="+number;
+   
+}
+function Ether(){
+    var price = prompt("Enter the amount");
+    var bit_price = 306700;
+    var number = bit_price % price;
+    document.getElementById("btn4").innerHTML="Total quantity you can perchase is ="+number;
+   
+}
